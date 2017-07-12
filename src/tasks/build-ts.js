f40e904607ef0d867430489f74da118efcc6881a
Chris Towles <Chris.Towles@gmail.com>
2017-07-12 00:12:05 -0400

    working webpack gulp

--- src/tasks/build-ts.js
+++ src/tasks/build-ts.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp');
-const plumber = require('gulp-plumber');
+
 const chokidar = require('chokidar');
+const size = require('gulp-size');
+const webpack = require('webpack-stream');
+
+/*
+const plumber = require('gulp-plumber');
 const ts = require('gulp-typescript');
 const tslint = require('gulp-tslint');
-const config = require('./includes/config.js');
-const messages = require('./includes/messages.js');
 const utils = require('./includes/utilities.js');
+const config = require('./includes/config.js');
+*/
 
+const messages = require('./includes/messages.js');
 
+/*
 function processThemeTs() {
   messages.logProcessFiles('build:ts');
   // pull in the project TypeScript config
@@ -21,17 +28,47 @@ function processThemeTs() {
     .pipe(tsProject());
 
   return tsResult.js
+    .pipe(size({
+      showFiles: true,
+      pretty: true,
+    }))
+    .pipe(webpack({
+      output: {
+        filename: 'webpack_output.js',
+      },
+    }))
     .pipe(gulp.dest(config.dist.assets));
+}
+*/
+
+
+function processTestJs() {
+  messages.logProcessFiles('build:ts');
+  return gulp.src('src/scripts_webpack/test123.js')
+    .pipe(size({
+      showFiles: true,
+      pretty: true,
+    }))
+    .pipe(webpack({
+      output: {
+        filename: 'testABC.js',
+      },
+    }))
+    .pipe(gulp.dest('dist/assets'));
 }
 
 gulp.task('build:ts', () => {
-  processThemeTs();
+  // processThemeTs();
+  processTestJs();
 });
 
 gulp.task('watch:ts', () => {
-  chokidar.watch([config.src.ts], {ignoreInitial: true})
+  // chokidar.watch([config.src.ts], {ignoreInitial: true})
+  chokidar.watch(['src/scripts_webpack/test123.js'], {ignoreInitial: true})
     .on('all', (event, path) => {
       messages.logFileEvent(event, path);
-      processThemeTs();
+      // processThemeTs();
+      processTestJs();
     });
 });
+
